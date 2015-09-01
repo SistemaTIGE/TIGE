39848a00b0afca4c46e6611a4ede02957b3c146b
mateusrodrigues <mateus.rodrigues@live.com>
2015-09-01 11:01:38 -0300

    Initial Commit.

---
+++ TIGE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TIGE.MvcApplication" Language="C#" %>
---
+++ TIGE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TIGE.Startup))]
+namespace TIGE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
